38868ad3d5086e39f4f5fc4aa14bcf5342d480ec
TonasP <priamothomaz@gmail.com>
2024-10-07 23:31:35 -0300

    Add files via upload

--- Algoritimo/menuprocoleguinha.js
+++ Algoritimo/menuprocoleguinha.js
@@ -2,6 +2,7 @@ const prompt = require("prompt-sync")();
 let contador= 0
 let pizzacalabresa = ["calabresa", "oregano", "mussarela"]
 function receita(){
+    console.log ("-------------------------------------------")
     console.log ("  Bem vindo ao livro de receitas ")
     console.log ("     Pizza de calabresa")
     console.log ("     O que deseja fazer ?")
@@ -10,50 +11,60 @@ function receita(){
     console.log ("  3. Remover item da receita")
     console.log ("  4. Ver quantos items vão na receita")
     console.log ("  5. Fechar o livro")
+    console.log ("-------------------------------------------")
     let escolha = parseInt(prompt("Por favor, escolha uma das opções acima"))
     switch (escolha){
-      case 1:
-        for (let i=0; i<pizzacalabresa.length; i++){
-            console.log (pizzacalabresa[i])}
+        case 1:
+            console.log("------------------------")
+            for (let i=0; i<pizzacalabresa.length; i++){
+                console.log (pizzacalabresa[i])}
+            console.log ("-----------------------")    
             return receita()
             
         
-      case 2:
+        case 2:
          for (let i=0; i<pizzacalabresa.length; i++){
             let adicionar = (prompt ("Digite o item que deseja adicionar, pressione 0 caso queira parar"))
-           let existente = pizzacalabresa.indexOf(adicionar)
-            if (existente==0){
-                console.log ("o item que desja adicionar já existe!")
-                break
-            }
-           if (adicionar=="0"){
-             return  receita()
-            } pizzacalabresa[pizzacalabresa.length]=adicionar}
-            break;
+            let existente = pizzacalabresa.indexOf(adicionar)
+                if (existente===0){
+                    console.log ("o item que deseja adicionar já existe!")
+                    return receita()
+                }
+                if (adicionar=="0"){
+                    return  receita()
+                } pizzacalabresa[pizzacalabresa.length]=adicionar}
+                    break;
         case 3:
-            let remover = prompt("Qual item deseja remover da receita?")
-            let procurar = pizzacalabresa.findIndex(item => item ==remover)
-            if (procurar !== -1){
-                pizzacalabresa.splice(procurar, 1)
-                console.log ("o item", remover, "foi removido")
-                return receita()
+            for (let i=0; i<pizzacalabresa.length; i++){
+                let remover = prompt("Qual item deseja remover da receita?, caso não queira remover mais nenhum item, deixe o campo em branco!")
+                let procurar = pizzacalabresa.findIndex(item => item ==remover)
+                    if (procurar !== -1){
+                        pizzacalabresa.splice(procurar, 1)
+                        console.log ("o item", remover, "foi removido")
+                
+                    }
+                    else{
+                        console.log ("O item inserido não existia, logo, não foi removido")
+                    } 
+                if (remover.length==0){
+                    break
+                }
             }
-             else{
-                console.log ("O item inserido não existia, logo, não foi removido")
-             }
-            return receita()
+        return receita()
     
         case 4:
-                 for (let i=0; i<pizzacalabresa.length; i++){
-                    contador++
-                 } console.log ("há", contador, "items nesta receita")
-                 return receita()
+            for (let i=0; i<pizzacalabresa.length; i++){
+             contador++
+            }  console.log ("há", contador, "items nesta receita")
+             contador=0   
+        return receita()
+
         case 5:
             console.log ("Você decidiu fechar o livro, tenha um bom dia caro cozinheiro!!")        
-            break 
+        break 
         default:
             console.log ("Insira uma opção valida")
-            return receita()
-        } 
+        return receita()
+    } 
      
 } receita()
