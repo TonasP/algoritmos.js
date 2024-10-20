048e0a254830a682800438d3fff21695e711d372
TonasP <priamothomaz@gmail.com>
2024-10-20 19:46:07 -0300

    Add files via upload

--- Algoritimo/cadastropjint.js
+++ Algoritimo/cadastropjint.js
@@ -24,15 +24,43 @@ function editar(){
      console.log ("A alteração de", coluna, "para", alteracao, "foi efetuada!")
     }
     function adicionar(){
-        for (let i=0; i<ficha.length; i++)
-            console.log (ficha[i])
-    }
+      let novoItem = {};
+      console.log ("Caso o queira deixar o espaço vazio, digite --")
+      novoItem.Nome = prompt("Insira o nome do calçado:");
+      novoItem.tipolinha1 = prompt("Insira o tipo de linha 1:");
+      novoItem.tipolinha2 = prompt("Insira o tipo de linha 2:");
+      novoItem.cor1 = prompt("Insira a cor 1:");
+      novoItem.cor2 = prompt("Insira a cor 2:");
+      novoItem.cor3 = prompt("Insira a cor 3:");
+      novoItem.lowmelt = prompt("Insira o lowmelt:");
+      novoItem.monofio1 = prompt("Insira o monofio 1:");
+      novoItem.monofio2 = prompt("Insira o monofio 2:");
+      novoItem.quantidade = prompt("Insira a quantidade:");
+  
+      ficha.push(novoItem);
+      console.log("Nova ficha adicionada com sucesso!");
+  }   
+    function remover(){
+      let nome= prompt("Qual o nome do calçado correspondente a ficha que quer remover?")
+      let indice= ficha.findIndex(f=> f.Nome= nome)
+     if (indice===-1){
+        console.log ("A ficha não foi encontrada!")
+        return menu()
+     }
+      
+        ficha.splice(indice, 1)
+        console.log ("A ficha cujo o nome era", nome, "foi removida")
+    
+      
+    }    
+
     function menu(){
         console.log ("Menu da ficha técnica")
         console.log ("  1. Visualizar fichas")
         console.log ("  2. Editar ficha ")
         console.log ("  3. Adicionar ficha ")
-        console.log ("  4. Sair do menu")
+        console.log ("  4. Remover ficha")
+        console.log ("  5. Sair do menu")
         opcao= parseInt(prompt("Qual ação deseja realizar?"))
     switch (opcao){
         case 1:
@@ -44,9 +72,20 @@ function editar(){
         case 3:
             adicionar()
             return menu()  
+        case 4:
+          remover()
+          return menu()
+           
+        case 5:
+          console.log("Você saiu do menu")
+          break
+
+        default:
+          console.log("Insira uma opção valida!")
+          return menu()    
+          }    
     }
-    }
-    while(opcao!==4){
+    while(opcao!==5){
         menu()
     }
     
