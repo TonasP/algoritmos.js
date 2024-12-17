d3c0b5b8b1bc3a77c2e99c8ed393f609b54af931
TonasP <priamothomaz@gmail.com>
2024-12-17 15:23:32 -0300

    Add files via upload

--- GymControl.js
+++ GymControl.js
@@ -8,6 +8,158 @@ const pool = new Pool({
     password: '1234',     // Substitua pela sua senha
     port: 5432                 // Porta do PostgreSQL
 });
+
+async function visualizar(){
+    try {
+        console.log ("1- Tabela de clientes")
+        console.log ("2- Tabela de funcionários")
+        console.log ("3- Tabela de serviços")
+        console.log ("4- Tabela de planos")
+        console.log ("5- Tabela de pagamentos")
+        console.log ("6- Retornar ao menu principal")
+        let opcao= parseInt(prompt("Selecione a tabela que deseja acessar!"))
+        switch (opcao){
+            case 1:
+                await vsclientes()
+                return visualizar()
+            case 2:
+                await vsfuncionarios()
+                return visualizar()
+            case 3: 
+                await vsservicos()
+                return visualizar()
+            case 4: 
+                await vsplanos()
+                return visualizar()
+            case 5: 
+                await vspagamentos()
+                return visualizar()
+            case 6: 
+                console.log("Retornando ao menu principal!")
+                return
+            default:
+                console.log("Opção inválida!")
+                return visualizar()
+        }
+    }
+    catch{
+        console.log ("Não foi possivel visualizar as tabelas!")
+    }
+}
+
+async function atualizar(){
+    
+}
+
+async function cadastrar(){
+
+    console.log("1- Cadastrar cliente");
+    console.log("2- Cadastrar funcionário");
+    console.log("3- Cadastrar serviço");
+    console.log("4- Cadastrar pagamento");
+    console.log("5- Retornar ao menu principal");
+    let opcao = parseInt(prompt("Selecione uma das opções"))
+
+switch (opcao) {
+    case 1:
+        await cadastrarcliente()
+        return cadastrar()
+    case 2:     
+        await cadastrarfuncionario
+        return cadastrar()
+      
+    case 3:
+        await cadastrarservico()
+        return cadastrar()
+        
+    case 4:
+        await adicionarpagamento()
+        return cadastrar()
+       
+    case 5:
+        console.log ("Retornando ao menu principal")
+        return
+        
+    default:
+        console.log("Opção inválida");
+}
+}
+
+async function procurar(){
+
+console.log("1- Procurar cliente");
+console.log("2- Procurar funcionário");
+console.log("3- Procurar serviço");
+console.log("4- Procurar pagamento");
+console.log("5- Retornar ao menu principal");
+
+let opcao = parseInt(prompt("Selecione uma das opções"))
+
+switch (opcao) {
+    case 1:
+        await procurarcliente()
+        return procurar()
+    case 2:
+        await procurarfuncionario()
+        return procurar()
+    case 3:
+        await procurarservico()
+        return procurar()
+    case 4:
+        await procurarpagamento()
+        return procurar()
+    case 5:
+        console.log("Retornando ao menu principal")
+        return
+    default:
+        console.log("Opção inválida");
+}
+}
+
+async function vspagamentos(){
+    try{
+        const query = 'select * from "GymControl".pagamentos'
+        const result = await pool.query(query)
+        console.table(result.rows)
+    }
+    catch{
+        console.log ("Erro")
+    }
+}
+
+async function vsplanos(){
+    try{
+        const query = 'select * from "GymControl".planos'
+        const result = await pool.query(query)
+        console.table(result.rows)
+    }
+    catch{
+        console.log ("Erro")
+    }
+}
+
+async function vsfuncionarios(){
+    try{
+        const query = 'select * from "GymControl".funcionarios'
+        const result = await pool.query(query)
+        console.table(result.rows)
+    }
+    catch{
+        console.log ("Erro")
+    }
+}
+
+async function vsservicos(){
+    try{
+        const query = 'select * from "GymControl".servicos'
+        const result = await pool.query(query)
+        console.table(result.rows)
+    }
+    catch{
+        console.log ("Erro")
+    }
+}
+
 async function vsclientes(){
     try{
         const query = 'select * from "GymControl".clientes'
@@ -19,22 +171,6 @@ async function vsclientes(){
     }
 }
 
-async function visualizar(){
-    try {
-        console.log ("1- Tabela de clientes")
-        console.log ("2- Tabela de funcionários")
-        console.log ("3- Tabela de serviços")
-        console.log ("4- Tabela de planos")
-        console.log ("5- Tabela de pagamentos")
-        let opcao= parseInt(prompt("Selecione a tabela que deseja acessar!"))
-        switch (opcao){
-            case 1:
-        }
-    }
-    catch{
-        console.log ("Não foi possivel visualizar as tabelas!")
-    }
-}
 
 async function procurarservico(){
     try{
@@ -46,6 +182,19 @@ async function procurarservico(){
     }
     catch{
         console.log ("Erro ao procurar o serviço")
+    }
+}
+
+async function procurarpagamento(){
+    try{
+        let id= procurarcliente()
+        let query = 'select pagamentos.* from "GymControl".pagamentos join "GymControl".servicos on servicos.id = pagamentos.id_servico join "GymControl".clientes on clientes.id = servicos.id_cliente where clientes.id = $1'
+        let result = await pool.query(query, [id])
+        console.table (result.rows)
+        return result.rows[0].id
+    }
+    catch{
+        console.log ("Erro ao procurar pagamento!")
     }
 }
 
@@ -137,16 +286,46 @@ async function cadastrarcliente(){
 }
 
 async function menu() {
-    console.log("1-");
-    console.log("2-");
-    console.log("3-");
-    console.log("4-");
-    console.log("5-");
-    console.log("6-");
-    console.log("7-");
-    console.log("8-");
-    console.log("9-");
-    console.log("10-");
+    console.log ("1- Visualizar");
+    console.log ("2- Procurar")
+    console.log ("3- Cadastrar")
+    console.log ("4- Atualizar")
+    console.log ("5- Deletar")
     let opcao = parseInt(prompt("Selecione uma das opções acima!"))
+    switch(opcao){
+        case 1: 
+            await visualizar()
+            return menu()
+        case 2:
+            await procurar()
+            return menu()
+        case 3:
+            await cadastrar() 
+            return menu()
+        case 4: 
+            await
+            return menu()
+        case 5:
+            await 
+            return menu()
+        case 6:
+            await 
+            return menu()
+        case 7: 
+            await 
+            return menu()
+        case 8: 
+            await 
+            return menu()
+        case 9:
+            await
+            return menu()
+        case 10:
+            console.log ("Saindo do menu")
+            break
+        default:
+            console.log ("Opção inválida!")        
+            return menu()
 
-}
+    }   
+} menu()
